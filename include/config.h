701998e404b7ee5afe14023f82042b2dff8f338b
Dan Cogliano <dan@zcontent.net>
2026-09-22 12:37:03 -0400

    build # replaced with version #

--- include/config.h
+++ include/config.h
@@ -475,7 +475,7 @@
 // perfectly matched an older decoder, and it took a full round of analysis to
 // realise the source and the firmware had diverged. Bump this whenever you
 // change something you intend to test.
-#define ACJ_BUILD_ID        "build 65"
+#define ACJ_BUILD_ID        "version 1.4"
 
 
 // ---------------------------------------------------------------------------
